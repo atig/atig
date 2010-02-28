058d56f0159e68952153f2f3b401c45f20b758f3
mzp <mzpppp@gmail.com>
2010-02-28 12:44:25 +0900

    timeline agentをちょっと賢くした

--- atig/agent/timeline.rb
+++ atig/agent/timeline.rb
@@ -10,17 +10,25 @@ module Atig
 
       def initialize(logger, api, db)
         @log = logger
+        @prev = nil
+        @first = true
         log :info, "initialize"
 
         @api = api
         @api.repeat(30) do|t|
-          statuses = t.get('/statuses/home_timeline')
+          q = { :count => 200 }
+          q.update(:since_id => @prev) if @prev
+          q.update(:count => 20) if @first
+          @first = false
+
+          statuses = t.get('/statuses/home_timeline', q)
 
           db.transaction do|t|
             statuses.reverse_each do|status|
               t.status.add :timeline, status
             end
           end
+          @prev = statuses[0].id if statuses && statuses.size > 0
         end
       end
     end
