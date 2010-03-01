d0e8d6b032f936214bc1c76119226c728df0ad40
mzp <mzpppp@gmail.com>
2010-03-01 18:27:30 +0900

    re implement sized array

--- atig/sized_array.rb
+++ atig/sized_array.rb
@@ -1,8 +1,8 @@
 #! /opt/local/bin/ruby -w
 # -*- mode:ruby; coding:utf-8 -*-
-
-class SizedArray
-  Roman = %w[
+module Atig
+  class SizedArray
+    Roman = %w[
 	  a   i   u   e   o  ka  ki  ku  ke  ko  sa shi  su  se  so
 	 ta chi tsu  te  to  na  ni  nu  ne  no  ha  hi  fu  he  ho
 	 ma  mi  mu  me  mo  ya      yu      yo  ra  ri  ru  re  ro
@@ -14,42 +14,45 @@ class SizedArray
 	rya     ryu     ryo
 	gya     gyu     gyo  ja      ju      jo bya     byu     byo
 	pya     pyu     pyo
-  ].freeze
+    ].freeze
 
-  def initialize(size)
-    @size = size
-    @index = 0
-    @xs = []
-    @seq = Roman
-    @tid = {}
-  end
+    def initialize(size)
+      @size = size
+      @index = 0
+      @xs = []
+      @seq = Roman
+      @tid = {}
+    end
 
-  def include?(id)
-    @xs.any?{|item| item.id == id }
-  end
+    def include?(id)
+      @xs.any?{|item| item.id == id }
+    end
 
-  def index(s)
-    @xs.index(s)
-  end
+    def index(s)
+      @xs.index(s)
+    end
 
-  def generate(n)
-    ret = []
-    begin
-      n, r = n.divmod(@seq.size)
-      ret << @seq[r]
-    end while n > 0
-    ret.reverse.join #.gsub(/n(?=[bmp])/, "m")
-  end
+    def push(status)
+      tid = generate @index
+      status[:tid] = tid
+      @tid[tid] = @xs[@index] = status
+      @index = (@index + 1) % @size
+    end
+    alias_method :<<, :push
 
-  def push(status)
-    tid = generate @index
-    status[:tid] = tid
-    @tid[tid] = @xs[@index] = status
-    @index = (@index + 1) % @size
-  end
-  alias_method :<<, :push
+    def [](tid)
+      @tid[tid]
+    end
 
-  def [](tid)
-    @tid[tid]
+    private
+    def generate(n)
+      ret = []
+      begin
+        n, r = n.divmod(@seq.size)
+        ret << @seq[r]
+      end while n > 0
+      ret.reverse.join #.gsub(/n(?=[bmp])/, "m")
+    end
+
   end
 end
