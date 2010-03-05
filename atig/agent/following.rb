718fbbafc218610fd0c690cfd453942d8fc201b5
mzp <mzpppp@gmail.com>
2010-03-05 18:14:00 +0900

    まちがて消したコードをもどした

--- atig/agent/following.rb
+++ atig/agent/following.rb
@@ -1,4 +1,26 @@
 #! /opt/local/bin/ruby -w
+# -*- mode:ruby; coding:utf-8 -*-
+
+require 'atig/util'
+
+module Atig
+  module Agent
+    class Following
+      include Util
+
+      def initialize(logger, api, db)
+        @log = logger
+        @db  = db
+        log :info, "initialize"
+
+        api.repeat(3600) do|t|
+          followers = t.page("followers/ids/#{@db.me.id}", :ids)
+
+          if @db.followings.empty?
+            friends = t.page("statuses/friends/#{@db.me.id}", :users)
+          else
+            @db.me = t.get("account/update_profile")
+            return if @db.me.friends_count == @db.followings.size
             friends = t.get("statuses/friends/#{@db.me.id}", :users)
           end
 
