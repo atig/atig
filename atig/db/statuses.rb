c516c9aad32de076c6187f48e18a462fd929d21f
mzp <mzpppp@gmail.com>
2010-03-16 10:34:02 +0900

    sqliteを使うようにした

--- atig/db/statuses.rb
+++ atig/db/statuses.rb
@@ -1,8 +1,9 @@
 #! /opt/local/bin/ruby -w
 # -*- mode:ruby; coding:utf-8 -*-
 require 'atig/db/listenable'
-require 'atig/db/sized_uniq_array'
+require 'sqlite3'
 require 'atig/db/roman'
+require 'base64'
 
 class OpenStruct
   def id; method_missing(:id) end
@@ -13,55 +14,77 @@ module Atig
     class Statuses
       include Listenable
 
-      def initialize(size)
-        @queue = SizedUniqArray.new size
+      def initialize(name)
+        @db_name = name
+        @id    = 0
         @roman = Roman.new
+
+        execute do|db|
+          db.execute %{create table status (
+                         id   text,
+                         tid  text,
+                         screen_name text,
+                         user_id     text,
+                         created_at  text,
+                         data blob);}
+        end
       end
 
-      def size; @queue.size end
-
       def add(opt)
-        entry = OpenStruct.new opt.merge(:id => opt[:status].id)
+        entry = OpenStruct.new opt.merge(:id  => opt[:status].id,
+                                         :tid => @roman.make(@id))
+        execute do|db|
+          return unless db.execute(%{SELECT id FROM status WHERE id = ?}, entry.id).empty?
 
-        if i = @queue.push(entry) then
-          entry.tid = @roman.make i
+          db.execute(%{INSERT INTO status
+                      VALUES(:id, :tid, :screen_name, :user_id, :created_at, :data)},
+                     :id          => entry.id,
+                     :tid         => entry.tid,
+                     :screen_name => opt[:user].screen_name,
+                     :user_id     => opt[:user].id,
+                     :created_at  => opt[:status].created_at,
+                     :data        => [Marshal.dump(entry)].pack('m'))
+          @id += 1
           notify entry
         end
       end
 
       def find_by_screen_name(name, opt={})
-        find(opt){|s| s.user.screen_name == name }
+        find 'screen_name',name, opt
       end
 
       def find_by_user(user, opt={})
-        find(opt){|s| s.user == user }
+        find 'user_id', user.id, opt
       end
 
       def find_by_tid(tid)
-        if i = @roman[tid] then
-          @queue[i]
-        end
+        find('tid', tid).first
       end
 
       def find_by_id(id)
-        @queue.find{|status| status.id == id }
+        find('id', id).first
       end
 
       private
-      def find(opt={},&f)
-        statuses = []
+      def find(lhs,rhs, opt={},&f)
+        res = []
+        execute do|db|
+          db.execute("SELECT data FROM status WHERE #{lhs} = ? ORDER BY created_at DESC LIMIT ?",
+                     rhs,opt.fetch(:limit,20)) do|data,*_|
+            res << Marshal.load(data.unpack('m').first)
+          end
+        end
+        res
+      end
 
-        g = if opt[:limit] then
-              lambda {|s|
-                break if opt[:limit] <= statuses.size
-                statuses << s if f.call(s)
-              }
-            else
-              lambda{|s| statuses << s if f.call(s) }
-            end
-
-        @queue.reverse_each(&g)
-        statuses
+      def execute(&f)
+        db = SQLite3::Database.new @db_name
+        begin
+          res = f.call db
+        ensure
+          db.close
+        end
+        res
       end
     end
   end
