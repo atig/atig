39d896d458e7632efcded32893a09cb4ba571c2b
mzp <mzpppp@gmail.com>
2010-03-20 11:48:31 +0900

    favoriteのバグを修正

--- atig/command/favorite.rb
+++ atig/command/favorite.rb
@@ -6,19 +6,16 @@ module Atig
   module Command
     class Favorite < Atig::Command::Command
       def initialize(*args); super end
-      def command_name; %r/\A(un)?fav\z/ end
+      def command_name; %w(fav unfav) end
 
       def action(target, mesg, command, args)
-        method   = command[1].nil? ? "create" : "destroy"
-        force    = !!command[2]
-        entered  = command[0].capitalize
-        p command
-        p method
+        method   = { 'fav' => 'create', 'unfav' => 'destroy' }[command]
+
         args.each do|tid|
           if entry = find_by_tid(tid)
             api.delay(0){|t|
-              res = t.post("favorites/#{method}/#{entry.id}")
-              yield "#{entered}: #{res.user.screen_name}: #{res.text}"
+              res = t.post("favorites/#{method}/#{entry.status.id}")
+              yield "#{command.upcase}: #{entry.user.screen_name}: #{entry.status.text}"
             }
           else
             yield "No such ID : #{tid}"
