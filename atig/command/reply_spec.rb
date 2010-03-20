05b1595b7ca57aff0f4cc27f4cd5b05f46e53698
mzp <mzpppp@gmail.com>
2010-03-20 11:00:52 +0900

    /me replyのテストを書いた

---
+++ atig/command/reply_spec.rb
@@ -0,0 +1,44 @@
+#! /opt/local/bin/ruby -w
+# -*- mode:ruby; coding:utf-8 -*-
+
+require 'atig/command/reply'
+require 'atig/command/command_helper'
+
+describe Atig::Command::Reply do
+  include CommandHelper
+  before do
+    @command = init Atig::Command::Reply
+
+    target = status 'blah blah', 'id'=>'1'
+    entry  = entry user(1,'mzp'), target
+    @res   = mock 'res'
+
+    @statuses.stub!(:find_by_tid).with('a').and_return(entry)
+  end
+
+  it "should have '/me status' name" do
+    @gateway.names.should == %w(mention re reply)
+  end
+
+  it "should post the status by API" do
+    @api.should_receive(:post).
+      with('statuses/update', {:status=>'abc @mzp', :in_reply_to_status_id=>'1'}).
+      and_return(@res)
+
+    call '#twitter', "reply", %w(a abc @mzp)
+
+    @gateway.updated.should  == [ @res, '#twitter', 'In reply to mzp: blah blah' ]
+    @gateway.filtered.should == { :status => 'abc @mzp', :in_reply_to_status_id=>'1'}
+  end
+
+  it "should add screen name as prefix" do
+    @api.should_receive(:post).
+      with('statuses/update', {:status=>'@mzp mzp', :in_reply_to_status_id=>'1'}).
+      and_return(@res)
+
+    call '#twitter', "reply", %w(a mzp)
+
+    @gateway.updated.should  == [ @res, '#twitter', 'In reply to mzp: blah blah' ]
+    @gateway.filtered.should == { :status => '@mzp mzp', :in_reply_to_status_id=>'1'}
+  end
+end
