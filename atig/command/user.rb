21050aefcefad85235b04dd4b5d99e370202a624
mzp <mzpppp@gmail.com>
2010-03-21 09:34:59 +0900

    エラーに強くした

--- atig/command/user.rb
+++ atig/command/user.rb
@@ -20,17 +20,22 @@ module Atig
 
         count = 20 unless (1..200).include?(count = num)
         api.delay(0) do|t|
-          statuses = t.get("statuses/user_timeline", { :count => count, :screen_name => nick})
-          db.transaction do|d|
-            statuses.reverse_each do|status|
-              d.statuses.add :status => status, :user => status.user, :source => :user
-            end
-            db.
-              statuses.
-              find_by_screen_name(nick, :limit=>count).
-              reverse_each do|entry|
+          begin
+            statuses = t.get("statuses/user_timeline",
+                             { :count => count, :screen_name => nick})
+            db.transaction do|d|
+              statuses.reverse_each do|status|
+                d.statuses.add :status => status, :user => status.user, :source => :user
+              end
+              db.
+                statuses.
+                find_by_screen_name(nick, :limit=>count).
+                reverse_each do|entry|
                 gateway[target].message entry, Net::IRC::Constants::NOTICE
               end
+            end
+          rescue Twitter::APIFailed => e
+            yield e.to_s
           end
         end
       end
