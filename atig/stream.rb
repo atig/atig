8321e35d2c0883c52b481f94a1589131049bf385
mzp <mzpppp@gmail.com>
2010-04-02 06:00:05 +0900

    streamにtimeout時間を追加した。

--- atig/stream.rb
+++ atig/stream.rb
@@ -25,25 +25,54 @@ module Atig
       uri.path += ".json"
       uri.query = query.to_query_str unless query.empty?
 
-      @log.debug [uri.to_s]
+      timeout = @opts.stream_timeout || 60*60
+      if timeout != 0 then
+        loop{
+          begin
+            streaming(uri, timeout, &f)
+          rescue TimeoutError
+          end
+        }
+      else
+        streaming(uri,&f)
+      end
+    end
+
+    def read(http, request, &f)
       buffer = ''
+      http.request(request) do |response|
+        unless response.code == '200' then
+          raise APIFailed,"#{response.code} #{response.message}"
+        end
+
+        response.read_body do |chunk|
+          next if chunk.strip.empty?
+          buffer << chunk
+          begin
+            while buffer =~ /\A.*?\r\n/ do
+              json    = $&
+              buffer  = $'
+              f.call TwitterStruct.make(JSON.parse(json))
+            end
+          rescue => e
+            @log.error e.inspect
+          end
+        end
+      end
+    end
+
+    def streaming(uri, read_time =nil,&f)
+      @log.debug [uri.to_s]
       Net::HTTP.start(uri.host, uri.port) do |http|
         request = Net::HTTP::Post.new uri.request_uri
         request.basic_auth @user, @password
-        http.request(request) do |response|
-          response.read_body do |chunk|
-            next if chunk.strip.empty?
-            buffer << chunk
-            begin
-              while buffer =~ /\A.*?\r\n/ do
-                json    = $&
-                buffer  = $'
-                f.call TwitterStruct.make(JSON.parse(json))
-              end
-            rescue => e
-              @log.error e.inspect
-            end
-          end
+
+        if read_time then
+          timeout( read_time ){
+            read(http, request,&f)
+          }
+        else
+          read(http, request,&f)
         end
       end
     end
