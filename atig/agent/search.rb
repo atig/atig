54305cf4006a7797188440b7f941a286615e215c
mzp <mzpppp@gmail.com>
2010-04-11 20:36:18 +0900

    saved search用のagentを追加

---
+++ atig/agent/search.rb
@@ -0,0 +1,23 @@
+#! /opt/local/bin/ruby -w
+# -*- mode:ruby; coding:utf-8 -*-
+
+require 'atig/util'
+
+module Atig
+  module Agent
+    class Search
+      include Util
+
+      def initialize(context, api, db)
+        @log = context.log
+        @db  = db
+        log :info, "initialize"
+
+        api.repeat(30) do|t|
+          searches = t.get "saved_searches"
+          @db.searches.update searches
+        end
+      end
+    end
+  end
+end
