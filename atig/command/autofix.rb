6225e02385f3edaf433d60d974c55e0d354cf2c7
mzp <mzpppp@gmail.com>
2010-04-15 05:10:46 +0900

    /me autofixのコードを整理

--- atig/command/autofix.rb
+++ atig/command/autofix.rb
@@ -12,13 +12,17 @@ module Atig
   module Command
     class Autofix < Atig::Command::Command
       def initialize(*args); super end
-      def command_name; %w(status) end
+      def command_name; /(?:autofix|topic|overwwrite)!?/ end
 
       def distance(s1, s2)
         c1 = s1.split //
         c2 = s2.split //
         distance = Atig::Levenshtein.levenshtein c1, c2
         distance.to_f / [ c1.size, c2.size ].max
+      end
+
+      def fix?(command, text, prev)
+        command[-1,1] == '!' or distance(text, prev.status.text) < 0.5
       end
 
       def action(target, mesg, command, args)
@@ -29,18 +33,23 @@ module Atig
         text = mesg.split(" ", 2)[1]
         q = gateway.output_message(:status => text)
 
-        api.delay(0, :retry=>3) do|t|
-          ret = t.post("statuses/update", q)
-          gateway.update_status ret, target
+        prev,*_ = db.statuses.find_by_user( db.me, :limit => 1)
 
-          previous,*_ = db.statuses.find_by_user( db.me, :limit => 1)
+        unless fix?(command, q[:status], prev) then
+          api.delay(0, :retry=>3) do|t|
+            ret = t.post("statuses/update", q)
+            gateway.update_status ret, target
+          end
+        else
+          api.delay(0, :retry=>3) do|t|
+            yield "Similar update in previous. Conclude that it has error."
+            yield "And overwrite previous as new status: #{q[:status]}"
 
-          if distance(q[:status], previous.status.text) < 0.5 then
-            t.post("statuses/destroy/#{previous.status.id}")
+            ret = t.post("statuses/update", q)
+            gateway.update_status ret, target
+            t.post("statuses/destroy/#{prev.status.id}")
             db.transaction{|d|
-              d.statuses.remove_by_id previous.id
-              yield "Similar update in previous. Conclude that it has error."
-              yield "And overwrite previous as new status: #{q[:status]}"
+              d.statuses.remove_by_id prev.id
             }
           end
         end
