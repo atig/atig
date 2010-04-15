3bdac4ad88fc3718e17a476cd0cfc0da59098bb2
mzp <mzpppp@gmail.com>
2010-04-16 06:02:32 +0900

    検索用チャンネルを動作可能な形にした

--- atig/channel/search.rb
+++ atig/channel/search.rb
@@ -6,36 +6,21 @@ module Atig
     class Search
       def initialize(context, gateway, db)
         @gateway  = gateway
-        @channels = {}
+        @channels = Hash.new do|hash,name|
+          channel = gateway.channel "#s:#{name}"
+          channel.join_me
+          hash[name] = channel
+        end
 
         db.searches.listen do|kind, s|
-          unless @channels.key? s.name then
-            @channels[s.name] = make s.name, s.query
-          end
-          @channels[s.name][:channel].send kind, []
+          @channels[s.name].send kind, []
         end
 
         db.statuses.listen do|entry|
           if entry.source == :search then
-            @channels[entry.search_name].message entry
-          else
-            @channels.each do|_, ch|
-              if entry.status.text.include?(ch[:query]) then
-                ch[:channel].message entry
-              end
-            end
+            @channels[entry.name].message entry
           end
         end
-      end
-
-      private
-      def make(name, query)
-        channel = @gateway.channel "#s:#{name}"
-        channel.join_me
-        {
-          :channel => channel,
-          :query   => query,
-        }
       end
     end
   end
