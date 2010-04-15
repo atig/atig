b43a202f931ad37c5791d75f867943ed5fdb5619
mzp <mzpppp@gmail.com>
2010-04-16 05:23:27 +0900

    Search用のAPIとコードを共有したいので、Twitterのコードの一部をBasicTwitterにくくりだした

---
+++ atig/basic_twitter.rb
@@ -0,0 +1,98 @@
+#!/usr/bin/env ruby
+# -*- coding: utf-8 -*-
+
+require "json"
+require 'atig/twitter_struct'
+require 'atig/http'
+
+module Atig
+  # from tig.rb
+  class BasicTwitter
+    attr_reader :limit, :remain
+
+    class APIFailed < StandardError; end
+
+    def initialize(context, api_base)
+      @log   = context.log
+      @opts  = context.opts
+      @api_base = api_base
+      @http  = Atig::Http.new @log
+
+      @limit = @remain = 150
+    end
+
+    def api(path, query = {}, opts = {})
+      path.sub!(%r{\A/+}, "")
+
+      uri = api_base
+      uri.path += path
+      uri.path += ".json" if path != "users/username_available"
+      uri.query = query.to_query_str unless query.empty?
+
+      header      = {}
+
+      begin
+        ret = request(uri, opts)
+      rescue OpenSSL::SSL::SSLError => e
+        @log.error e.inspect
+        raise e.inspect
+      end
+
+      if ret["X-RateLimit-Limit"] then
+        hourly_limit = ret["X-RateLimit-Limit"].to_i
+        unless hourly_limit.zero?
+          if @limit != hourly_limit
+            msg = "The rate limit per hour was changed: #{@limit} to #{hourly_limit}"
+            @log.info msg
+            @limit = hourly_limit
+          end
+        end
+      end
+
+      if ret["X-RateLimit-Remaining"] then
+        @remain = ret["X-RateLimit-Remaining"].to_i
+        @log.debug "IP based limit: #{@remain}"
+      end
+
+      case ret
+      when Net::HTTPOK # 200
+        # Avoid Twitter's invalid JSON
+        json = ret.body.strip.sub(/\A(?:false|true)\z/, "[\\&]")
+
+        res = JSON.parse(json)
+        if res.is_a?(Hash) && res["error"] # and not res["response"]
+          if @error != res["error"]
+            @error = res["error"]
+            log @error
+          end
+          raise APIFailed, res["error"]
+        end
+
+        TwitterStruct.make(res)
+      when Net::HTTPNoContent,  # 204
+        Net::HTTPNotModified # 304
+        []
+      when Net::HTTPBadRequest # 400: exceeded the rate limitation
+        if ret.key?("X-RateLimit-Reset")
+          @log.info "waiting for rate limit reset"
+          s = ret["X-RateLimit-Reset"].to_i - Time.now.to_i
+          if s > 0
+            sleep (s > 60 * 10) ? 60 * 10 : s # 10 分に一回はとってくるように
+          end
+        end
+        raise APIFailed, "#{ret.code}: #{ret.message}"
+      when Net::HTTPUnauthorized # 401
+        raise APIFailed, "#{ret.code}: #{ret.message}"
+      else
+        raise APIFailed, "Server Returned #{ret.code} #{ret.message}"
+      end
+    rescue Errno::ETIMEDOUT, JSON::ParserError, IOError, Timeout::Error, Errno::ECONNRESET => e
+      raise APIFailed, e.inspect
+    end
+
+    private
+    def api_base
+      URI(@opts.send @api_base)
+    end
+  end
+end
--- atig/twitter.rb
+++ atig/twitter.rb
@@ -3,22 +3,16 @@
 
 require "json"
 require 'atig/twitter_struct'
+require 'atig/basic_twitter'
 require 'atig/http'
 
 module Atig
   # from tig.rb
-  class Twitter
-    attr_reader :limit, :remain
-
-    class APIFailed < StandardError; end
-
+  class Twitter < BasicTwitter
     def initialize(context, oauth)
-      @log   = context.log
-      @opts  = context.opts
+      super context, :api_base
       @oauth = oauth
       @http  = Atig::Http.new @log
-
-      @limit = @remain = 150
     end
 
     # authenticate = trueでないとSSL verified errorがでることがある
@@ -49,154 +43,19 @@ END
     end
     http_methods :get, :post, :put, :delete
 
-
-    def api(path, query = {}, opts = {})
-      path.sub!(%r{\A/+}, "")
-
+    protected
+    def request(uri, opts)
       authenticate = opts.fetch(:authenticate, true)
-      method = opts.fetch(:method, :get)
-
-      uri = api_base(authenticate)
-      uri.path += path
-      uri.path += ".json" if path != "users/username_available"
-      uri.query = query.to_query_str unless query.empty?
+      method       = opts.fetch(:method, :get)
 
       header      = {}
       req         = @http.req method, uri, header
-
       @log.debug [req.method, uri.to_s]
-      begin
-        if authenticate
-          ret = oauth 30, req
-        else
-          ret = http(uri, 30, 30).request req
-        end
-      rescue OpenSSL::SSL::SSLError => e
-        @log.error e.inspect
-        raise e.inspect
-      end
-
-      if ret["X-RateLimit-Limit"] then
-        hourly_limit = ret["X-RateLimit-Limit"].to_i
-        unless hourly_limit.zero?
-          if @limit != hourly_limit
-            msg = "The rate limit per hour was changed: #{@limit} to #{hourly_limit}"
-            @log.info msg
-            @limit = hourly_limit
-          end
-        end
-      end
-
-      if ret["X-RateLimit-Remaining"] then
-        @remain = ret["X-RateLimit-Remaining"].to_i
-        @log.debug "IP based limit: #{@remain}"
-      end
-
-      case ret
-      when Net::HTTPOK # 200
-        # Avoid Twitter's invalid JSON
-        json = ret.body.strip.sub(/\A(?:false|true)\z/, "[\\&]")
-
-        res = JSON.parse(json)
-        if res.is_a?(Hash) && res["error"] # and not res["response"]
-          if @error != res["error"]
-            @error = res["error"]
-            log @error
-          end
-          raise APIFailed, res["error"]
-        end
-
-        TwitterStruct.make(res)
-      when Net::HTTPNoContent,  # 204
-        Net::HTTPNotModified # 304
-        []
-      when Net::HTTPBadRequest # 400: exceeded the rate limitation
-        if ret.key?("X-RateLimit-Reset")
-          @log.info "waiting for rate limit reset"
-          s = ret["X-RateLimit-Reset"].to_i - Time.now.to_i
-          if s > 0
-            sleep (s > 60 * 10) ? 60 * 10 : s # 10 分に一回はとってくるように
-          end
-        end
-        raise APIFailed, "#{ret.code}: #{ret.message}"
-      when Net::HTTPUnauthorized # 401
-        raise APIFailed, "#{ret.code}: #{ret.message}"
+      if authenticate
+        oauth 30, req
       else
-        raise APIFailed, "Server Returned #{ret.code} #{ret.message}"
+        @http.http(uri, 30, 30).request req
       end
-    rescue Errno::ETIMEDOUT, JSON::ParserError, IOError, Timeout::Error, Errno::ECONNRESET => e
-      raise APIFailed, e.inspect
-    end
-
-    private
-    def server_name
-      "twittergw"
-    end
-
-    def server_version
-      @server_version ||= instance_eval {
-        head = `git rev-parse HEAD 2>/dev/null`.chomp
-        head.empty?? "unknown" : head
-      }
-    end
-
-
-    def http(uri, open_timeout = nil, read_timeout = 60)
-      http = case
-             when @httpproxy
-               Net::HTTP.new(uri.host, uri.port, @httpproxy.address, @httpproxy.port,
-                             @httpproxy.user, @httpproxy.password)
-             when ENV["HTTP_PROXY"], ENV["http_proxy"]
-               proxy = URI(ENV["HTTP_PROXY"] || ENV["http_proxy"])
-               Net::HTTP.new(uri.host, uri.port, proxy.host, proxy.port,
-                             proxy.user, proxy.password)
-             else
-               Net::HTTP.new(uri.host, uri.port)
-             end
-      http.open_timeout = open_timeout if open_timeout # nil by default
-      http.read_timeout = read_timeout if read_timeout # 60 by default
-      if uri.is_a? URI::HTTPS
-        http.use_ssl     = true
-        http.cert_store = @cert_store
-        http.verify_mode = OpenSSL::SSL::VERIFY_PEER
-      end
-      http
-    rescue => e
-      @log.error e
-    end
-
-    def http_req(method, uri, header = {}, credentials = nil)
-      accepts = ["*/*;q=0.1"]
-      types   = { "json" => "application/json", "txt" => "text/plain" }
-      ext     = uri.path[/[^.]+\z/]
-      accepts.unshift types[ext] if types.key?(ext)
-      user_agent = "#{self.class}/#{server_version} (#{File.basename(__FILE__)}; net-irc) Ruby/#{RUBY_VERSION} (#{RUBY_PLATFORM})"
-
-      header["User-Agent"]      ||= user_agent
-      header["Accept"]          ||= accepts.join(",")
-      header["Accept-Charset"]  ||= "UTF-8,*;q=0.0" if ext != "json"
-
-      req = case method.to_s.downcase.to_sym
-            when :get
-              Net::HTTP::Get.new    uri.request_uri, header
-            when :head
-              Net::HTTP::Head.new   uri.request_uri, header
-            when :post
-              Net::HTTP::Post.new   uri.path,        header
-            when :put
-              Net::HTTP::Put.new    uri.path,        header
-            when :delete
-              Net::HTTP::Delete.new uri.request_uri, header
-            else # raise ""
-            end
-      if req.request_body_permitted?
-        req["Content-Type"] ||= "application/x-www-form-urlencoded"
-        req.body = uri.query
-      end
-      req.basic_auth(*credentials) if credentials
-      req
-    rescue => e
-      @log.error e
     end
 
     def api_base(secure = true)
