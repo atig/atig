f58c54768310477fae54a37679598a03ba329514
mzp <mzpppp@gmail.com>
2010-07-08 06:37:17 +0900

    user streamに対応した

---
+++ atig/agent/user_stream.rb
@@ -0,0 +1,32 @@
+#! /opt/local/bin/ruby -w
+# -*- mode:ruby; coding:utf-8 -*-
+require 'atig/util'
+
+module Atig
+  module Agent
+    class UserStream
+      include Util
+
+      def initialize(context, api, db)
+        @log = context.log
+        @api = api
+        @prev = nil
+
+        return unless context.opts.stream
+
+        log :info, "initialize"
+
+        @api.stream do|t|
+          t.watch('user') do |status|
+            @log.debug status.inspect
+            if status and status.user
+              db.transaction do|d|
+                d.statuses.add :status => status, :user => status.user, :source => :user_stream
+              end
+            end
+          end
+        end
+      end
+    end
+  end
+end
