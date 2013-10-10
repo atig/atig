ea060baf9162a2f90c34beb64d23109f59e99adf
Yamagishi Kazutoshi <ykzts@desire.sh>
2013-10-10 10:00:38 +0900

    Add interval option into the home timeline Agent
    ref: #37

--- lib/atig/agent/timeline.rb
+++ lib/atig/agent/timeline.rb
@@ -4,11 +4,18 @@ require 'atig/agent/agent'
 module Atig
   module Agent
     class Timeline < Atig::Agent::Agent
+      DEFAULT_INTERVAL = 60
+
       def initialize(context, api, db)
-        return if context.opts.stream
+        @opts = context.opts
+        return if @opts.stream
         super
       end
-      def interval; 30 end
+
+      def interval
+        @interval ||= @opts.interval.nil? ? DEFAULT_INTERVAL : @opts.interval.to_i
+      end
+
       def path; '/statuses/home_timeline' end
       def source; :timeline end
     end
